a7ce70b98f297e9597071ca9030b3d42b1397f5a
Powerlated <zjia08@gmail.com>
2020-08-21 17:25:00 -0400

    MBC3: Use switch statements for address decoding

--- src/core/mbc/mbc3.ts
+++ src/core/mbc/mbc3.ts
@@ -20,93 +20,104 @@ export default class MBC3 extends MBCWithRTC {
     rtcLatchVal = 0;
 
     read8(addr: number): number {
-        if (addr >= 0xA000 && addr <= 0xBFFF) {
-            if (this.sramEnable) {
-                switch (this.ramBank) {
-                    case 0x0:
-                    case 0x1:
-                    case 0x2:
-                    case 0x3:
-                        let index = (addr - 0xA000) + (this.ramBank * 0x2000);
-                        return this.sram[index];
+        switch (addr >> 12) {
+            case 0xA:
+            case 0xB:
+                if (this.sramEnable) {
+                    switch (this.ramBank) {
+                        case 0x0:
+                        case 0x1:
+                        case 0x2:
+                        case 0x3:
+                            let index = (addr - 0xA000) + (this.ramBank * 0x2000);
+                            return this.sram[index];
 
-                    case 0x8:
-                        return this.rtcSecondsLatch;
-                    case 0x9:
-                        return this.rtcMinutesLatch;
-                    case 0xA:
-                        return this.rtcHoursLatch;
-                    case 0xB:
-                        return (this.rtcDaysLatch >> 0) & 0xFF;
-                    case 0xC:
-                        let val = 0;
-                        val |= (this.rtcDaysLatch >> 8) & 0x01;
-                        val |= this.rtc.halted ? BIT_6 : 0;
-                        val |= this.rtc.daysOverflow ? BIT_7 : 0;
-                        return val;
+                        case 0x8:
+                            return this.rtcSecondsLatch;
+                        case 0x9:
+                            return this.rtcMinutesLatch;
+                        case 0xA:
+                            return this.rtcHoursLatch;
+                        case 0xB:
+                            return (this.rtcDaysLatch >> 0) & 0xFF;
+                        case 0xC:
+                            let val = 0;
+                            val |= (this.rtcDaysLatch >> 8) & 0x01;
+                            val |= this.rtc.halted ? BIT_6 : 0;
+                            val |= this.rtc.daysOverflow ? BIT_7 : 0;
+                            return val;
+                    }
+                } else {
+                    return 0xFF;
                 }
-            } else {
-                return 0xFF;
-            }
         }
         return 0xFF;
     }
 
     write8(addr: number, val: number): void {
-        if (addr >= 0x0000 && addr <= 0x1FFF) {
-            this.sramEnable = (val & 0xF) == 0xA;
-        }
-        else if (addr >= 0x2000 && addr <= 0x3FFF) {
-            this.romBank = val;
-        } else if (addr >= 0x4000 && addr <= 0x5FFF) {
-            this.ramBank = val;
-        } else if (addr >= 0xA000 && addr <= 0xBFFF) {
-            if (this.sramEnable) {
-                switch (this.ramBank) {
-                    case 0x0:
-                    case 0x1:
-                    case 0x2:
-                    case 0x3:
-                        this.sramDirty = true;
-                        let index = (addr - 0xA000) + (this.ramBank * 0x2000);
-                        this.sram[index] = val;
-                        break;
-
-                    case 0x8:
-                        console.log(`set seconds: ${val}`);
-                        this.rtc.setSeconds(val % 60);
-                        break;
-                    case 0x9:
-                        console.log(`set minutes: ${val}`);
-                        this.rtc.minutes = val % 60;
-                        break;
-                    case 0xA:
-                        console.log(`set hours: ${val}`);
-                        this.rtc.hours = val % 24;
-                        break;
-                    case 0xB:
-                        this.rtc.days &= ~0xFF;
-                        this.rtc.days |= (val << 0) & 0xFF;
-                        break;
-                    case 0xC:
-                        this.rtc.days &= ~0x100;
-                        this.rtc.days |= (val << 8) & 0x100;
-                        this.rtc.halted = bitTest(val, 6);
-                        this.rtc.daysOverflow = bitTest(val, 7);
-                        break;
+        switch (addr >> 12) {
+            case 0x0:
+            case 0x1:
+                this.sramEnable = (val & 0xF) == 0xA;
+                break;
+            case 0x2:
+            case 0x3:
+                this.romBank = val;
+                break;
+            case 0x4:
+            case 0x5:
+                this.ramBank = val;
+                break;
+            case 0x6:
+            case 0x7:
+                val &= 1;
+                if (this.rtcLatchVal == 0 && val == 1) {
+                    this.rtcSecondsLatch = this.rtc.seconds;
+                    this.rtcMinutesLatch = this.rtc.minutes;
+                    this.rtcHoursLatch = this.rtc.hours;
+                    this.rtcDaysLatch = this.rtc.days;
                 }
-            }
-        } else if (addr >= 0x6000 && addr <= 0x7FFF) {
-            val &= 1;
-            if (this.rtcLatchVal == 0 && val == 1) {
-                this.rtcSecondsLatch = this.rtc.seconds;
-                this.rtcMinutesLatch = this.rtc.minutes;
-                this.rtcHoursLatch = this.rtc.hours;
-                this.rtcDaysLatch = this.rtc.days;
-            }
-            this.rtcLatchVal = val;
+                this.rtcLatchVal = val;
+                break;
+            case 0xA:
+            case 0xB:
+                if (this.sramEnable) {
+                    switch (this.ramBank) {
+                        case 0x0:
+                        case 0x1:
+                        case 0x2:
+                        case 0x3:
+                            this.sramDirty = true;
+                            let index = (addr - 0xA000) + (this.ramBank * 0x2000);
+                            this.sram[index] = val;
+                            break;
+
+                        case 0x8:
+                            // console.log(`set seconds: ${val}`);
+                            this.rtc.setSeconds(val % 60);
+                            break;
+                        case 0x9:
+                            // console.log(`set minutes: ${val}`);
+                            this.rtc.minutes = val % 60;
+                            break;
+                        case 0xA:
+                            // console.log(`set hours: ${val}`);
+                            this.rtc.hours = val % 24;
+                            break;
+                        case 0xB:
+                            this.rtc.days &= ~0xFF;
+                            this.rtc.days |= (val << 0) & 0xFF;
+                            break;
+                        case 0xC:
+                            this.rtc.days &= ~0x100;
+                            this.rtc.days |= (val << 8) & 0x100;
+                            this.rtc.halted = bitTest(val, 6);
+                            this.rtc.daysOverflow = bitTest(val, 7);
+                            break;
+                    }
+                }
+                break;
         }
-        return;
     }
 
     getOffset() {
