68b940d1b801dc4dad2a0b1ae2d094749e0be16a
Powerlated <zjia08@gmail.com>
2021-11-05 04:10:41 -0400

    DSP: remove extra varible in feedback comb filter

--- src/core/dsp/feedback_comb_filter.ts
+++ src/core/dsp/feedback_comb_filter.ts
@@ -14,16 +14,15 @@ export class FeedbackCombFilter {
     }
 
     process(val: number): number {
-        let nextBufferPos = this.bufferPos;
-        if (++nextBufferPos >= this.buffer.length) nextBufferPos = 0;
-        let delayLineOut = this.buffer[nextBufferPos];
+        this.buffer[this.bufferPos] = val + this.lowpassBuf * this.decay;
+
+        if (++this.bufferPos >= this.buffer.length) this.bufferPos = 0;
+        let delayLineOut = this.buffer[this.bufferPos];
 
         // lowpass
         // this.lowpassBuf = (1 - this.lowpassBuf) / (1 - this.lowpassBuf / this.lowpass);
         this.lowpassBuf = delayLineOut; // TODO: maybe lowpass makes it sound better?
 
-        this.buffer[this.bufferPos] = val + this.lowpassBuf * this.decay;
-        this.bufferPos = nextBufferPos;
         return delayLineOut;
     }
 }
